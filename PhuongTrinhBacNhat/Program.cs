27b57eaed2dfc86886e7de4ff94275776022187b
lythanhngodev <lythanhngodev@gmail.com>
2017-12-05 00:20:41 +0700

    ./
    ./

---
+++ PhuongTrinhBacNhat/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuongTrinhBacNhat
+{
+    public class ptbn
+    {
+        public bool BatLoi(string x)
+        {
+            try
+            {
+                float a = float.Parse(x);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public float Doi(string x)
+        {
+            return float.Parse(x);
+        }
+        public string GPT(float a, float b)
+        {
+            if (a != 0)
+            {
+                return "co nghiem: " + (-b/a);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    return "vo so nghiem";
+                }
+                else
+                {
+                    return "vo nghiem";
+                }
+            }
+        }
+        string a, b;
+        float _a, _b;
+        static void Main(string[] args)
+        {
+            ptbn pt = new ptbn();
+            nhaplai:
+            Console.Write(" - Nhap he so a: ");
+            pt.a = Console.ReadLine();
+            Console.Write(" - Nhap he so b: ");
+            pt.b = Console.ReadLine();
+            if (!pt.BatLoi(pt.a)||!pt.BatLoi(pt.b))
+            {
+                Console.WriteLine("\t:( Nhap sai he so!");
+                goto nhaplai;
+            }
+            else
+            {
+                pt._a = pt.Doi(pt.a);
+                pt._b = pt.Doi(pt.b);
+                Console.WriteLine("Phuong trinh "+pt.GPT(pt._a, pt._b));
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ PhuongTrinhBacNhatTests/ptbnTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhuongTrinhBacNhat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuongTrinhBacNhat.Tests
+{
+    [TestClass()]
+    public class ptbnTests
+    {
+        [TestMethod()]
+        // bat loi nhap xuat
+        public void BatLoiTest()
+        {
+            Assert.AreEqual(new ptbn().BatLoi("a"), false);
+            Assert.AreEqual(new ptbn().BatLoi("10"), true);
+        }
+
+        [TestMethod()]
+        // bat loi doi chuoi sang so (float)
+        public void DoiTest()
+        {
+            Assert.AreEqual(new ptbn().Doi("3.3"), 3.3f);
+        }
+
+        [TestMethod()]
+        public void GPTTest()
+        {
+            Assert.AreEqual(new ptbn().GPT(1.0f, 2.0f), "co nghiem: -2");
+            Assert.AreEqual(new ptbn().GPT(0.0f, 2.0f), "vo nghiem");
+            Assert.AreEqual(new ptbn().GPT(0.0f, 0.0f), "vo so nghiem");
+        }
+    }
+}
